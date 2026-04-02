0fb2b0601a4900395a1ef09ba76c6ed2cf081cf4
H4CKTHOR <84154887+H4CKTHOR@users.noreply.github.com>
2026-04-02 12:48:22 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,44 @@
-echo "Wait please"
+clear
+figlet "H4CKTHOR HUB FULL PENTEST SUITE"
+echo "[Contact]>: https://h4ckthor.github.io/website"
 sleep 2
-echo "ok."
+echo "[i]> Wait please..."
 sleep 2
-echo "Fixing..."
+echo "[i]> Status check: [X]"
+sleep 3
+clear
+figlet H4CKTHOR HUB FULL PENTEST SUITE
+echo "[Contact]>: https://h4ckthor.github.io/website"
+echo "[i]> Wait please..."
+sleep 2
+echo "[i]> Status: [Ok]"
+sleep 2
+clear
+figlet H4CKTHOR HUB FULL PENTEST SUITE
+echo "[Contact]>: https://h4ckthor.github.io/website"
+echo "[Fixing...]"
+sleep 1
+echo "[Dont close terminal!]"
+sleep 2
 sudo apt-get install python3-tk -y
 python3 -m venv venv
 source venv/bin/activate
 pip install customtkinter requests beautifulsoup4 urllib3 --break-system-packages
-echo "Fixed!"
-echo "Wait Tool starting."
+sleep 2
+clear
+sleep 2
+figlet Fixed
+sleep 2
+echo "~\_Wait Tool starting._/~"
 sleep 3
+clear
+figlet "H4CKTHOR HUB FULL PENTEST SUITE"
+echo "[Version]>: Monster Edition Premium Version 1.0"
+echo "[Contact]>: https://h4ckthor.github.io/website"
+echo "[i]> Dont close terminal!"
+sleep 2
 python3 thor-suite.py
+clear
+sleep 2
+figlet "BYE BYE ;)"
+echo "[Contact]>: https://h4ckthor.github.io/website"
